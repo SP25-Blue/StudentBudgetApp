5bdc3cc5402aa24e26fa8e889fdf78d0d21e79e4
larezelliot <elliotlarez02@gmail.com>
2023-09-07 15:16:58 -0400

    Added sample .html, .js, .css

---
+++ Website/script.js
@@ -0,0 +1,7 @@
+// Get a reference to the button element by its id
+const alertButton = document.getElementById('alertButton');
+
+// Add a click event listener to the button
+alertButton.addEventListener('click', function () {
+    alert('Button clicked! This is your alert message.');
+});
