4b11c0ec5c723073f64f63e5b0852a48f819a2d7
Jade McAlister <mcalister.vjade@gmail.com>
2023-10-24 15:42:58 -0400

    resized images

--- MobileApp/SpendSense/app/(tabs)/three.tsx
+++ MobileApp/SpendSense/app/(tabs)/three.tsx
@@ -1,18 +1,22 @@
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, ScrollView } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 
 export default function TabThreeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Three</Text>
-      <Image source ={require('../../assets/images/test/sample_budget_pie-chart.png')}></Image>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/three.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.title}>Dashboard</Text>
+        <Image source ={require('../../assets/images/test/sample_budget_pie-chart.png')} style={styles.smallImage}/>
+        <Image source ={require('../../assets/images/test/sample_monthly_spending.png')} style={styles.smallImage}/>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <EditScreenInfo path="app/(tabs)/three.tsx" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -31,5 +35,14 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  smallImage: {
+    width: '100%',
+    resizeMode: 'contain'
+  },
+  
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
   },
 });
