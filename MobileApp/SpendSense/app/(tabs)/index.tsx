35635c6a80c2124cf7b6c3c2155443778c7aee28
regvsn <reginald.appiah7@gmail.com>
2023-10-26 10:43:48 -0400

    Created Sections For Tab One

--- MobileApp/SpendSense/app/(tabs)/index.tsx
+++ MobileApp/SpendSense/app/(tabs)/index.tsx
@@ -1,32 +1,61 @@
-import { StyleSheet } from 'react-native';
+import { ScrollView, SectionList, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+      <Text style={styles.title}>Home</Text>
+      <View style={styles.container}>
+        <SectionList
+          sections={[
+            { title: 'You have saved: $100 this week!', data: ['Saved $25 on gas', 'Saved $75 on groceries'] },
+            {
+              title: 'You gained 15 points this week! \n (Ranked #4 out of 10 friends)',
+              data: [
+                'Gained 5 points for using our grocery discount',
+                'Gained 9 points for spending 50% less on gas',
+                'Gained 1 point for spending 20% less on clothing',
+              ],
+
+            },
+          ]}
+          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({ section }) => (
+            <Text style={styles.sectionHeader}>{section.title}</Text>
+          )}
+          keyExtractor={item => `basicListEntry-${item}`}
+        />
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'green'
+    flex: 2,
+    paddingTop: 22,
   },
   title: {
-    fontSize: 20,
+    fontSize: 30,
+    textAlign: 'center',
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  sectionHeader: {
+    paddingTop: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 20,
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
   },
 });
