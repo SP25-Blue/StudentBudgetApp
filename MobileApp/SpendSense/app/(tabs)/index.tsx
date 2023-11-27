f18f162b808145d4fb49ff907091eeb9a916a6bf
larezelliot <elliotlarez02@gmail.com>
2023-11-26 20:08:46 -0500

    HomePage edit

--- MobileApp/SpendSense/app/(tabs)/index.tsx
+++ MobileApp/SpendSense/app/(tabs)/index.tsx
@@ -5,33 +5,56 @@ import {
 } from 'react-native';
 
 import { textStyles, viewStyles } from '../../constants/Styles';
+import { ApplicationService } from '../../core/services/ApplicationService';
+import { User } from '../../core/user/User'
+import { Link } from 'expo-router';
 
 export default function TabOneScreen() {
 
-  return (
-    <View style={viewStyles.container}>
-      <Text style={textStyles.title}>Home</Text>
-      <View style={viewStyles.container}>
-        <SectionList
-          sections={[
-            {
-              title: 'You have saved: $100 this week!',
-              data: ['Saved $25 on gas', 'Saved $75 on groceries']
-            },
-            {
-              title: 'You gained 15 points this week! \n (Ranked #4 out of 10 friends)',
-              data: [
-                'Gained 5 points for using our grocery discount',
-                'Gained 9 points for spending 50% less on gas',
-                'Gained 1 point for spending 20% less on clothing',
-              ],
-            },
-          ]}
-          renderSectionHeader={({ section }) => <Text style={textStyles.sectionHeader}>{section.title}</Text>}
-          renderItem={({ item }) => <Text style={textStyles.item}>{item}</Text>}
-          keyExtractor={item => `basicListEntry-${item}`}
-        />
+  const user = ApplicationService.activeUser;
+
+  if (user === undefined) {
+    return (
+      <View>
+        <Text style={textStyles.text}>
+          <Link href={'/pages/page_createAccount'}>
+            [Sign Up]
+          </Link>
+          or
+          <Link href={'/pages/page_authentication'}>
+            [Log In]
+          </Link>
+        </Text>
       </View>
-    </View >
-  );
+    );
+  }
+  else {
+    return (
+      <View style={viewStyles.container}>
+        <Text style={textStyles.title}>Home</Text>
+        <Text style={textStyles.title}>Welcome, {user.username}</Text>
+        <View style={viewStyles.container}>
+          <SectionList
+            sections={[
+              {
+                title: 'You have saved: $100 this week!',
+                data: ['Saved $25 on gas', 'Saved $75 on groceries']
+              },
+              {
+                title: 'You gained 15 points this week! \n (Ranked #4 out of 10 friends)',
+                data: [
+                  'Gained 5 points for using our grocery discount',
+                  'Gained 9 points for spending 50% less on gas',
+                  'Gained 1 point for spending 20% less on clothing',
+                ],
+              },
+            ]}
+            renderSectionHeader={({ section }) => <Text style={textStyles.sectionHeader}>{section.title}</Text>}
+            renderItem={({ item }) => <Text style={textStyles.item}>{item}</Text>}
+            keyExtractor={item => `basicListEntry-${item}`}
+          />
+        </View>
+      </View >
+    );
+  }
 }
