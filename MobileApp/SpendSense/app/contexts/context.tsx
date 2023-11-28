d8a2054b18e11a1e6e1a323f5e4a61c9e741298d
larezelliot <elliotlarez02@gmail.com>
2023-11-28 14:39:33 -0500

    Create Context for Ads

--- MobileApp/SpendSense/app/contexts/context.tsx
+++ MobileApp/SpendSense/app/contexts/context.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useContext, useState } from "react"
 import { User } from "../../core/user/User"
+import { Advertising } from "../../core/ads/advertising";
 
 interface UserContextType {
     user: User | undefined;
@@ -35,6 +36,51 @@ export const useUser = (): UserContextType => {
     const context = useContext(UserContext);
     if (!context) {
         throw new Error('useUser must be used within a UserProvider');
+    }
+
+    return context;
+};
+
+
+interface AdsContextType {
+    ads: Advertising[];
+    uploadAd: (ad: Advertising) => void;
+    removeAd: (ad: Advertising) => void;
+}
+
+export const AdsContext = createContext<AdsContextType | null>(null);
+
+export const AdsProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [ads, setAds] = useState<Advertising[]>(new Array<Advertising>());
+
+    const uploadAd = (adData: Advertising | undefined) => {
+        if (adData === undefined) {
+            return;
+        }
+        ads?.push(adData);
+    };
+
+    const removeAd = (adData: Advertising | undefined) => {
+        if (adData === undefined) {
+            return;
+        }
+        let index = ads?.findIndex((ad) => ad !== adData);
+
+        if (index !== undefined && index > 0)
+            ads?.at(index);
+    };
+
+    return (
+        <AdsContext.Provider value={{ ads, uploadAd, removeAd }}>
+            {children}
+        </AdsContext.Provider>
+    );
+};
+
+export const useAds = (): AdsContextType => {
+    const context = useContext(AdsContext);
+    if (!context) {
+        throw new Error('useAds must be used within a AdsProvider');
     }
 
     return context;
