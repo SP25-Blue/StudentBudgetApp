f529591334a369ae8481381f3a0c5a07659ed93a
larezelliot <elliotlarez02@gmail.com>
2023-11-29 18:04:06 -0500

    Updated Discover Page

--- MobileApp/SpendSense/app/(tabs)/page_discover.tsx
+++ MobileApp/SpendSense/app/(tabs)/page_discover.tsx
@@ -23,15 +23,45 @@ export default function PageDiscoverScreen() {
         <View style={[viewStyles.clearContainer, { width: '30%' }]}>
             <Pressable
                 onPress={() => Linking.openURL(item.url)}>
-                <Image
+                <ImageBackground style={{ height: 144, width: '100%' }}
                     source={{ uri: item.imagePath }}
-                    style={{ height: 144, width: '100%' }}
-                    resizeMode="contain"
-                />
-                <Text>Hola Mundo</Text>
-            </Pressable>
-        </View>
+                    resizeMode='cover'
+                    blurRadius={4}>
+                    <Image style={{ height: '100%', width: '100%' }}
+                        source={{ uri: item.imagePath }}
+                        resizeMode="contain"
+
+                    />
+                </ImageBackground>
+                <View style={{ height: 52, width: '100%', flexDirection: 'row', justifyContent: 'space-around', overflow: 'hidden' }}>
+                    <Text style={{ width: '20%' }} > {item.name}</Text>
+                    <Text style={{ width: '80%' }} numberOfLines={3}>{item.description}</Text>
+                </View>
+            </Pressable >
+        </View >
     ));
+
+    const renderAd_YourAdHere = (
+        <View style={[viewStyles.clearContainer, { width: '30%' }]}>
+            <Pressable
+                onPress={() => { router.push("/pages/page_createAdvertising") }}>
+                <ImageBackground style={{ height: 144, width: '100%' }}
+                    source={require('../../assets/images/Advertising/your_ad_here.jpg')}
+                    resizeMode='cover'
+                    blurRadius={4}>
+                    <Image style={{ height: '100%', width: '100%' }}
+                        source={require('../../assets/images/Advertising/your_ad_here.jpg')}
+                        resizeMode="contain"
+
+                    />
+                </ImageBackground>
+                <View style={{ height: 52, width: '100%', flexDirection: 'row', justifyContent: 'space-around', overflow: 'hidden' }}>
+                    <Text style={{ width: '20%' }} > Your Add Here </Text>
+                    <Text style={{ width: '80%' }} numberOfLines={3}>Click here to add your ad to SpendSense!</Text>
+                </View>
+            </Pressable >
+        </View >
+    );
 
     return (
         <ImageBackground style={imageStyles.background}
@@ -46,15 +76,7 @@ export default function PageDiscoverScreen() {
                             justifyContent: 'space-around'
                         }}>
                         {renderedAds}
-                        <Pressable style={[viewStyles.clearContainer, { width: '30%', }]}
-                            onPress={() => { router.push("/pages/page_createAdvertising") }}>
-                            <Image
-                                source={require('../../assets/images/Advertising/your_ad_here.jpg')}
-                                style={{ height: 144, width: '100%' }}
-                                resizeMode="cover"
-                            />
-                            <Text>Create your own advertising!</Text>
-                        </Pressable>
+                        {renderAd_YourAdHere}
                     </View>
                 </ScrollView>
             </View >
