cd38810d0f3491f36a917ea8f33dc635aaf1cad7
larezelliot <elliotlarez02@gmail.com>
2023-12-03 16:57:00 -0500

    Finished Discover Page

--- MobileApp/SpendSense/app/(tabs)/page_discover.tsx
+++ MobileApp/SpendSense/app/(tabs)/page_discover.tsx
@@ -14,69 +14,67 @@ import { Advertising } from '../../core/ads/Advertising';
 import { Link, router } from 'expo-router';
 import { ScrollView } from 'react-native-gesture-handler';
 
-type ItemProps = { ad: Advertising };
+const AdvertisingItem = (ad: Advertising) => (
+    <View style={{ width: '100%', padding: 8 }}>
+        <Pressable
+            onPress={() => { Linking.openURL(ad.url) }}>
+            <Text style={[textStyles.title, { backgroundColor: '#FFFFFF', borderTopLeftRadius: 16, borderTopRightRadius: 16 }]}
+                numberOfLines={1} > {ad.name} </Text>
+            <ImageBackground style={{ height: 144, width: '100%', backgroundColor: '#FFFFFF' }}
+                source={{ uri: ad.imagePath }}
+                resizeMode='cover'
+                blurRadius={4}>
+                <Image style={{ height: '100%', width: '100%' }}
+                    source={{ uri: ad.imagePath }}
+                    resizeMode="contain"
+                />
+            </ImageBackground>
+            <Text style={[textStyles.text, { backgroundColor: '#F0F0F0', padding: 8 }]}
+                numberOfLines={3}> {ad.description} </Text>
+        </Pressable >
+    </View >
+);
+
+
+const YourAdHere_Element = (
+    <View style={{ width: '100%', padding: 8 }}>
+        <Pressable
+            onPress={() => { router.push("/pages/page_createAdvertising") }}>
+            <Text style={[textStyles.title, { backgroundColor: '#FFFFFF', borderTopLeftRadius: 16, borderTopRightRadius: 16 }]}
+                numberOfLines={1} > Your add here </Text>
+            <ImageBackground style={{ height: 144, width: '100%', backgroundColor: '#FFFFFF' }}
+                source={require('../../assets/images/Advertising/your_ad_here.jpg')}
+                resizeMode='cover'
+                blurRadius={4}>
+                <Image style={{ height: '100%', width: '100%' }}
+                    source={require('../../assets/images/Advertising/your_ad_here.jpg')}
+                    resizeMode="contain"
+                />
+            </ImageBackground>
+            <Text style={[textStyles.text, { backgroundColor: '#F0F0F0', padding: 8 }]}
+                numberOfLines={3}> Click here to add your ad to SpendSense! </Text>
+        </Pressable >
+    </View >
+);
 
 export default function PageDiscoverScreen() {
     const ads = useAds().ads;
-
-    const renderedAds = ads.map((item, index) => (
-        <View style={[viewStyles.clearContainer, { width: '30%' }]}>
-            <Pressable
-                onPress={() => { Linking.openURL(item.url) }}>
-                <Text style={textStyles.title}
-                    numberOfLines={1} > {item.name} </Text>
-                <ImageBackground style={{ height: 144, width: '100%' }}
-                    source={{ uri: item.imagePath }}
-                    resizeMode='cover'
-                    blurRadius={4}>
-                    <Image style={{ height: '100%', width: '100%' }}
-                        source={{ uri: item.imagePath }}
-                        resizeMode="contain"
-                    />
-                </ImageBackground>
-                <Text style={textStyles.text}
-                    numberOfLines={3}> {item.description} </Text>
-            </Pressable >
-        </View >
-    ));
-
-    const renderAd_YourAdHere = (
-        <View style={[viewStyles.clearContainer, { height: 288, width: '30%', overflow: 'hidden' }]}>
-            <Pressable
-                onPress={() => { router.push("/pages/page_createAdvertising") }}>
-                <Text style={textStyles.title}
-                    numberOfLines={1} > Your Ad Here </Text>
-                <ImageBackground style={{ height: 144, width: '100%' }}
-                    source={require('../../assets/images/Advertising/your_ad_here.jpg')}
-                    resizeMode='cover'
-                    blurRadius={4}>
-                    <Image style={{ height: '100%', width: '100%' }}
-                        source={require('../../assets/images/Advertising/your_ad_here.jpg')}
-                        resizeMode="contain"
-                    />
-                </ImageBackground>
-                <Text style={textStyles.text}
-                    numberOfLines={3}> Click here to add your ad to SpendSense! </Text>
-            </Pressable >
-        </View >
-    );
 
     return (
         <ImageBackground style={imageStyles.background}
             source={require('../../assets/images/Backgrounds/Leaf.png')}>
             <View style={viewStyles.container}>
-                <ScrollView style={{ width: '100%' }}>
-                    <View style={
-                        {
-                            width: '100%',
-                            flexDirection: 'row',
-                            flexWrap: 'wrap',
-                            justifyContent: 'space-around'
-                        }}>
-                        {renderedAds}
-                        {renderAd_YourAdHere}
-                    </View>
-                </ScrollView>
+                <FlatList style={{ width: '100%' }}
+                    ListHeaderComponent={<View></View>}
+                    ListFooterComponent={YourAdHere_Element}
+                    data={ads}
+                    renderItem={({ item }) =>
+                        <AdvertisingItem
+                            name={item.name}
+                            description={item.description}
+                            imagePath={item.imagePath}
+                            url={item.url} />}
+                    keyExtractor={item => item.name + '\n' + item.description} />
             </View >
         </ImageBackground >
     );
